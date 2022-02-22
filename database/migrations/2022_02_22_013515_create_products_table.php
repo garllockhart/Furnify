8ece078616ebc5b1e17ff570362736cfe0f295b1
Garly Nugraha <98641173+garllockhart@users.noreply.github.com>
2022-02-22 09:00:35 +0700

    Change Migration Product

--- database/migrations/2022_02_22_013515_create_products_table.php
+++ database/migrations/2022_02_22_013515_create_products_table.php
@@ -15,6 +15,11 @@ return new class extends Migration
     {
         Schema::create('products', function (Blueprint $table) {
             $table->id();
+            $table->string('name');
+            $table->bigInteger('price');
+            $table->longText('description');
+            $table->string('slug')->unique();
+            $table->softDeletes();
             $table->timestamps();
         });
     }
