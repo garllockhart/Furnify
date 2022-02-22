1999133018567bc8583425427805aa98cc30895b
Garly Nugraha <98641173+garllockhart@users.noreply.github.com>
2022-02-22 09:19:52 +0700

    Change Migration Product Gallery

--- database/migrations/2022_02_22_020437_create_product_galleries_table.php
+++ database/migrations/2022_02_22_020437_create_product_galleries_table.php
@@ -15,6 +15,10 @@ return new class extends Migration
     {
         Schema::create('product_galleries', function (Blueprint $table) {
             $table->id();
+            $table->bigInteger('products_id');
+            $table->string('url');
+            $table->boolean('is_featured')->default(false);
+            $table->softDeletes();
             $table->timestamps();
         });
     }
